fd17de388d2db4b6d3da86e26c55e75bd6c1d5ac
vinodrajalingam <vinodrajalingam@gmail.com>
2022-05-25 15:55:35 +0530

    Initial commit

---
+++ src/main/java/srv/demo/sprinboot/PlainspringbootApplication.java
@@ -0,0 +1,13 @@
+package srv.demo.sprinboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlainspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlainspringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/srv/demo/sprinboot/PlainspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package srv.demo.plainsprinboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlainspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
